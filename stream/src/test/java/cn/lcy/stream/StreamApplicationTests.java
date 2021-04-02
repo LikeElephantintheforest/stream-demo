1216b7ac911a4b075936473650bedfb139e94f19
李晨阳 <lichenyang@shizhuang-inc.com>
2021-04-02 15:21:51 +0800

    add:create moudle

---
+++ stream/src/test/java/cn/lcy/stream/StreamApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.lcy.stream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
